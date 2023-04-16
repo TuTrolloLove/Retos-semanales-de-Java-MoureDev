72a814f57b0b77ade1578e8ea1a102147b18507f
Bono0000 <jose542.ceus@gmail.com>
2023-04-16 22:03:56 +0200

    Reto 5

---
+++ Reto-5-HOLA-MUNDO/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().helloWorld());
+    }
+}
